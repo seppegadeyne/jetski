b717de1f058e31acc893504f41ac29dd46ef78a4
Seppe Gadeyne <seppe@fushia.be>
2025-03-21 17:20:46 +0100

    update

---
+++ quai-miner/run-quai-miner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LOG_FILE="$HOME/quai-miner/miner.log"
+MINER_EXECUTABLE="$HOME/quai-miner/quai-gpu-miner-nvidia-latest"
+
+# Check if the log file exists, if so, empty it
+if [ -f "$LOG_FILE" ]; then
+    > "$LOG_FILE"
+fi
+
+$MINER_EXECUTABLE -U -P stratum://suomi.straffesites.be:3333 --api-port 9090 >> "$LOG_FILE" 2>&1 &
